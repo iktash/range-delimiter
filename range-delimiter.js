7054f4e3b3c6684ae2373f32951b728756957ff4
ilja-jomedia <ilja@jomediainc.com>
2014-05-08 17:03:31 +0300

    change directive bindings and add demo

--- range-delimiter.js
+++ range-delimiter.js
@@ -95,88 +95,101 @@ app.service("Slider", function() {
     }
 });
 
-app.directive("rangeDelimiter", function($parse, RangePart, Slider, $timeout) {
-    var ranges = [];
-    var sliders = [];
+app.directive("rangeDelimiter", function(RangePart, Slider, $timeout) {
+    
 
     return {
         restrict: "E",
         replace: true,
         template: "<div class='range-cont'></div>",
-        require: "ngModel",
-        compile: function(elem, attrs) {
-            var parts = $parse(attrs.parts)();
-
-            var length = Object.keys(parts).length;
-            var count = 0;
-            for (var title in parts) {
-                count++;
-
-                var width = (100 - length) / length + "%";
-                var height = "20px";
+        scope: {
+            ranges: "="
+        },
+        link: function(scope, elem, attrs) {
+            var init = function() {
+                var ranges = [];
+                var sliders = [];
+                var parts = scope.ranges;
+                var length = Object.keys(parts).length;
+                var count = 0;
                 
-                if (typeof parts[title] === "string") {
-                    var background = parts[title];
-                } else {
-                    background = parts[title]['background'];
+                for (var title in parts) {
+                    count++;
+
+                    var width = (100 - length) / length + "%";
+                    var height = "20px";
                     
-                    if (parts[title]['width']) {
-                         width = parts[title]['width'];
+                    if (typeof parts[title] === "string") {
+                        var background = parts[title];
+                    } else {
+                        background = parts[title]['background'];
+                        
+                        if (parts[title]['width']) {
+                            width = parts[title]['width'];
+                        }
+
+                        if (parts[title]['height']) {
+                            height = parts[title]['height'];
+                        }
                     }
 
-                    if (parts[title]['height']) {
-                        height = parts[title]['height'];
+                    var new_elem = RangePart.create(background, width, height);
+
+                    elem.append(new_elem);
+
+                    ranges.push(new_elem);
+
+                    if (count < length) {
+
+                        var slider = Slider.create(new_elem, attrs.sliderClass);
+
+                        elem.append(slider);
+
+                        sliders.push(slider);
                     }
-                }
 
-                var new_elem = RangePart.create(background, width, height);
-
-                elem.append(new_elem);
-
-                ranges.push(new_elem);
-
-                if (count < length) {
-
-                    var slider = Slider.create(new_elem, attrs.sliderClass);
-
-                    elem.append(slider);
-
-                    sliders.push(slider);
-                }
-
-                if (count > 1) {
-                    sliders[count - 2].next = new_elem;
-                }
-            }
-
-            return function (scope, elem, attrs, ngModel) {
-                if (! ngModel) {
-                    return;
+                    if (count > 1) {
+                        sliders[count - 2].next = new_elem;
+                    }
                 }
 
                 angular.element(document).bind("mouseup", function(e) {
                     angular.element(document).unbind("mousemove");
                 });
 
-                scope.widths = [];
-                scope.applyWidths = function() {
-                    ngModel.$setViewValue(scope.widths);
-                }
-
                 $timeout(function() {
-                    for (var i = 0; i < ranges.length; i++) {
-                        scope.widths[i] = ranges[i].rect().width;
+                    for (var i = 0; i < scope.ranges.length; i++) {
+                        scope.ranges[i].range_width = ranges[i].rect().width;
 
                         (function(i) {
                             ranges[i].updateModel = function(width) {
-                                scope.widths[i] = width;
-                                scope.$apply(scope.applyWidths);
+                                scope.$apply(function() {
+                                    scope.ranges[i].range_width = width;
+                                });
                             }
                         })(i);
                     };
-                    scope.applyWidths();
                 });
             }
+
+            scope.$watchCollection(
+                function($scope) {
+                    var res = [];
+                    for (var i = 0; i < $scope.ranges.length; i++) {
+                        res.push($scope.ranges[i].background);
+                        res.push($scope.ranges[i].height);
+                        res.push($scope.ranges[i].width);
+                    }
+
+                    return res;
+                }
+                , function() {
+                    elem.empty();
+                    init();
+                }
+            );
+
+            init();
         }
     }
 });
